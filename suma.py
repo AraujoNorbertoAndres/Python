317ebeca0ca51a2119c57c5f68ff7156c7618974
Andres <andres.n.araujo@gmail.com>
2022-05-04 12:57:43 -0300

    Se agrega el proyecto suma

---
+++ suma.py
@@ -0,0 +1,11 @@
+print("SUMA")
+
+numero_uno = 3  #se declara la variable y se inicializa 
+numero_dos = 7  #se declara la variable y se inicializa 
+
+#se declara la variable 'resultado' posteriormente se hace
+#referencia a las variables anteriormente inicializadas con sus respectivos nombres.
+
+resultado = numero_uno + numero_dos 
+
+print(resultado)
